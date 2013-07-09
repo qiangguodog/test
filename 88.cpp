15764a59ab67bc47afce55828e7b66c547d66771
Paul Qiang <paul.qiang@nokia.com>
2013-07-09 10:44:36 +0800

    first commit

---
+++ 88.cpp
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+int main(void)
+{
+	//int i = 0xF;
+	//printf("%o\n", i);
+
+	/*
+	 printf usage:
+	 %d 十进制输出
+	 %x 十六进制输出
+	 %o 八进制输出
+    */
+
+	char i = 'A';
+
+	printf("%c\n",i);
+
+
+
+}
